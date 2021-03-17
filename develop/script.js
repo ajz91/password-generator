cf605e416fe0d76fe169c4f9a35eccab8eb2392d
AJ Ziarek <deltrigger@pm.me>
2021-03-17 12:31:29 -0500

    file changes, js changes

---
+++ develop/script.js
@@ -0,0 +1,18 @@
+//assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//  var passLength = window.prompt("Length of password? 8-128 characters");
